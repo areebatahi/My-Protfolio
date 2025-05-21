ac87189db3664d1af1eb3d0206b5674d8e1942ae
areebatahi <areebatahirarain@gmail.com>
2025-05-21 07:25:45 +0500

    protfolio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Footer from "./components/Footer"
+import IconSidebar from "./components/IconSidebar"
+
+function App() {
+
+  return (
+    <>
+      {/* <Sidebar /> */}
+      <IconSidebar />
+      {/* <Footer /> */}
+    </>
+  )
+}
+
+export default App
