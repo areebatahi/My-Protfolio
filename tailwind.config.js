0a0782a4c727007c4d7d61709d1e9b2fab84912f
areebatahi <areebatahirarain@gmail.com>
2025-06-13 20:34:01 +0500

    complete profolio

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Make sure your files are included
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      textColor: ['hover'], // Usually enabled by default
+    },
+  },
+  plugins: [],
+}
